16c25c5fe4c02fcbfb711b3850a4b23ddad6417f
Marek Polak <marazt@users.noreply.github.com>
2020-12-16 21:19:53 +0100

    Update insulin_dosage.py

--- backend/insulin/insulin_dosage.py
+++ backend/insulin/insulin_dosage.py
@@ -8,11 +8,15 @@ def recommended_insulin(
         tddi: float,
         target_glycemia: float,
         current_glycemia: float,
-        expected_carbohydrate_intake: grams
+        expected_carbohydrate_intake: grams,
+        insuline_type: XXX - vlastnost pacienta, enum,
+        historicke_davky: list historickych davek (puze behem poslednich 5 hodin)
 ) -> float:
+    
     dosage_carbohydrate_intake = expected_carbohydrate_intake / (CARBS_RATIO_NUMERATOR / tddi)
     dosage_target_glycemia = (current_glycemia - target_glycemia) / (INSULIN_SENSIBILITY_NUMERATOR / tddi)
-    return max(dosage_carbohydrate_intake + dosage_target_glycemia, 0)
+    ... sectu posledni davky davky rychleho insilinu a odectu od nasledujiciho radku.
+    return max(dosage_carbohydrate_intake + dosage_target_glycemia - NOVE_VYPOCITANA_HODNOTA, 0)
 
 
 if __name__ == '__main__':
