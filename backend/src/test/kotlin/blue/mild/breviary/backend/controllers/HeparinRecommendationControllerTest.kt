aa041cdde887d5ffa1f3007e26156043f2d12fef
carishkaa <carishkaa@gmail.com>
2021-01-13 18:22:39 +0100

    test failed

--- backend/src/test/kotlin/blue/mild/breviary/backend/controllers/HeparinRecommendationControllerTest.kt
+++ backend/src/test/kotlin/blue/mild/breviary/backend/controllers/HeparinRecommendationControllerTest.kt
@@ -10,16 +10,19 @@ import org.springframework.http.HttpEntity
 import org.springframework.http.HttpStatus
 import org.springframework.test.annotation.DirtiesContext
 import java.time.Instant
+import kotlin.test.assertNotNull
 
 class HeparinRecommendationControllerTest(
     @Autowired private val heparinPatientService: HeparinPatientService
 ) : ControllerTest() {
 
+    // OK
     @Test
     @DirtiesContext
     fun `should create heparin recommendation`(){
         signup()
         authenticateUser()
+
         val heparinPatient = heparinPatientService.createHeparinPatient(
             HeparinPatientDtoIn(
                 patient = PatientDtoIn(
@@ -28,7 +31,7 @@ class HeparinRecommendationControllerTest(
                     dateOfBirth = Instant.now(),
                     height = 180,
                     sex = Sex.FEMALE,
-                    otherParams = hashMapOf()
+                    otherParams = hashMapOf("param1" to "x")
                 ),
                 targetApttLow = 1.5f,
                 targetApttHigh = 2f,
@@ -38,7 +41,7 @@ class HeparinRecommendationControllerTest(
             )
         )
 
-        executeClientPost(
+        val heparinRecommendation = executeClientPost(
             "$apiPrefix/${ApiRoutes.HEPARIN_RECOMMENDATION}",
             setOf(HttpStatus.OK),
             typeReference<HeparinRecommendationDtoOut>(),
@@ -51,6 +54,81 @@ class HeparinRecommendationControllerTest(
             )
         )
 
+        assertNotNull(heparinRecommendation.body)
+        assert(heparinRecommendation.body!!.actualHeparinBolusDosage == 0.0f)
+    }
+
+
+    // FAILED (json parse errors)
+    @Test
+    @DirtiesContext
+    fun `should create heparin recommendation for new patient`(){
+        signup()
+        authenticateUser()
+
+        // create new heparin patient
+        val heparinPatient = executeClientPost(
+            "$apiPrefix/${ApiRoutes.HEPARIN_PATIENTS}",
+            setOf(HttpStatus.OK),
+            typeReference<HeparinPatientDtoOut>(),
+            HttpEntity<Any>(
+                HeparinPatientDtoIn(
+                    patient = PatientDtoIn(
+                        firstName = "Test",
+                        lastName = "Patient",
+                        dateOfBirth = Instant.now(),
+                        height = 180,
+                        sex = Sex.FEMALE,
+                        otherParams = hashMapOf("param1" to "x")
+                    ),
+                    targetApttLow = 1.5f,
+                    targetApttHigh = 2f,
+                    solutionHeparinUnits = 25_000f,
+                    solutionMilliliters = 500f,
+                    weight = 83f
+                ),
+                getAuthHeaders()
+            )
+        )
+
+        // create heparin recommendation
+        executeClientPost(
+            "$apiPrefix/${ApiRoutes.HEPARIN_RECOMMENDATION}",
+            setOf(HttpStatus.OK),
+            typeReference<HeparinRecommendationDtoOut>(),
+            HttpEntity<Any>(
+                HeparinRecommendationDtoIn(
+                    heparinPatientId = heparinPatient.body!!.id,
+                    currentAptt = 1.1f
+                ),
+                getAuthHeaders()
+            )
+        )
+    }
+
+
+    // FAILED (json parse error)
+    @Test
+    @DirtiesContext
+    fun `should not create heparin recommendation when patient does not exist`(){
+        signup()
+        authenticateUser()
+
+        val invalidID = "MzB8aGVwYXJpbi1wYXRpZW50"
+
+        // create heparin recommendation
+        executeClientPost(
+            "$apiPrefix/${ApiRoutes.HEPARIN_RECOMMENDATION}",
+            setOf(HttpStatus.BAD_REQUEST),
+            typeReference<HeparinRecommendationDtoOut>(),
+            HttpEntity<Any>(
+                HeparinRecommendationDtoIn(
+                    heparinPatientId = invalidID,
+                    currentAptt = 1.1f
+                ),
+                getAuthHeaders()
+            )
+        )
     }
 }
 
